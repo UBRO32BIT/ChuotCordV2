85180d42441c9845e4d3ec98288e2d58e01d1130
UBRO32BIT <90371372+UBRO32BIT@users.noreply.github.com>
2025-02-16 16:00:45 +0700

    fix guild invite authorization

--- server/src/routes/v1/guildInvite.route.js
+++ server/src/routes/v1/guildInvite.route.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const guildInviteController = require('../../controllers/v1/guildInvite.controller')
 const {checkAccessToken: CheckAuth} = require('../../middlewares/auth');
-const { AuthorizeGuildMember } = require('../../middlewares/guild');
 
 const router = express.Router({ mergeParams: true });
 
 //router.get('/', CheckAuth, guildInviteController.GetChannelsByGuildId);
-router.get('/:inviteCode', CheckAuth, AuthorizeGuildMember, guildInviteController.GetInviteByCode);
+router.get('/:inviteCode', CheckAuth, guildInviteController.GetInviteByCode);
 router.post('/:inviteCode', CheckAuth, guildInviteController.AddMemberByInviteCode);
 
 module.exports = router;
